c29ef6c88608001f0c88e44e8492682c16560d8e
Olusegun <shegoj@yahoo.com>
2025-11-30 20:54:46 +0000

    Updated terraform code to create web machine with ports 80 and 22 open

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend  "s3" {
-   bucket = "techbleat-cicd-state-bucket"
-   key    = "envs/dev/terraform.tfstate"
-   region = "eu-west-1"
-   encrypt = true
+  backend "s3" {
+    bucket  = "techbleat-cicd-state-bucket"
+    key     = "envs/dev/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
 
   }
   required_version = ">= 1.6.0"
@@ -20,15 +20,63 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# -------------------------
+# Web Node Security Group
+# -------------------------
 
-resource "aws_instance" "nginx-node" {
+resource "aws_security_group" "web_sg" {
+
+  name        = "web-sg"
+  description = "Allow SSH and Port 80  inbound, all outbound"
+  vpc_id      = "vpc-0a1624f291bfb283f"
+
+
+  # inbound SSH
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # inbound 80 (web)
+  ingress {
+    description = "Web port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security_group"
+  }
+
+}
+
+#-------------------------
+# EC2 Instances
+# ------------------------
+
+
+resource "aws_instance" "web-node" {
   ami                    = "ami-08b6a2983df6e9e25"
   instance_type          = "t3.micro"
   subnet_id              = "subnet-060ba13bd6800a0db"
-  vpc_security_group_ids = ["sg-090804d4ff518079d"]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = "MasterClass2025"
 
   tags = {
-    Name = "terraform-nginx-node"
+    Name = "web-node"
   }
 }
